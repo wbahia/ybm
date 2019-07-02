f8834c9838b9afcc14b2ce5f9c8f71f270717fca
wbahia <walter@CPDBOOK17>
2019-07-01 21:39:49 -0300

    Adjustin

--- src/app/panel/movies-mock.ts
+++ src/app/panel/movies-mock.ts
@@ -1,9 +1,9 @@
 import { Movie } from '../shared/movie.model'
 
 export const MOVIES : Array<Movie> = [
-    new Movie('Star Wars - Episode V - Empire Strikes Back', 'Movie', true, 'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg'),
+    new Movie('Star Wars - Episode V - Empire Strikes Back', 'Movie', false, 'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg'),
     new Movie('Star Wars - Episode IV - A New Hope', 'Movie', false, ''),
-    new Movie('Back to The Future', 'Movie', false, ''),
+    new Movie('Back to The Future', 'Movie', true, 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'),
     new Movie('The Goonies', 'Movie', false, ''),
     new Movie('Star Wars - Episode VI - Return of the Jedi', 'Movie', false, ''),
     new Movie('The Matrix', 'Movie', false, ''),
@@ -12,10 +12,10 @@ export const MOVIES : Array<Movie> = [
     new Movie('The Avengers - EndGame', 'Movie', false, ''),
     new Movie('The Avengers - Infinity War', 'Movie', false, ''),
     new Movie('Iron Man', 'Movie', false, ''),
-    new Movie('Friends', 'Series', true, 'https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'),
+    new Movie('Friends', 'Series', false, 'https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'),
     new Movie('BBT', 'Series', false, ''),
     new Movie('Game of Thrones', 'Series', false, ''),
-    new Movie('Lost', 'Series', false, ''),
+    new Movie('Lost', 'Series', true, 'https://m.media-amazon.com/images/M/MV5BNzhlY2E5NDUtYjJjYy00ODg3LWFkZWQtYTVmMzU4ZWZmOWJkXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'),
     new Movie('WestWorld', 'Series', false, ''),
     new Movie('Dark', 'Series', false, '')
        
